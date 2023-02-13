35c502cf85250c8ebc7c516816452e8258396d1f
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2023-02-13 10:43:24 +0800

    WIP minimize

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
+#![feature(allocator_api)]
 
-use tokio::sync::RwLock;
-use agent_config::ConfigStruct;
+use std::net::SocketAddr;
 
-mod agent_config;
-mod status_service;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::Response;
+use hyper::body::Bytes;
+use tokio::net::TcpListener;
 
-
-#[tokio::main]
-async fn main() {
-    let global_config = Arc::new(RwLock::new(agent_config::initial_config()));
-
-    tokio::spawn(start_web_server(global_config.clone()));
-
+async fn handle_req() -> Result<Response<BoxBody<Bytes, hyper::Error>>, Box<dyn std::error::Error + Send + Sync>> {
+    todo!()
 }
+
+pub async fn start_web_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::task::spawn(async move {
+            http1::Builder::new()
+                .serve_connection(stream, service_fn(|_| handle_req()))
+                .await;
+        });
+    }
+}
+
+fn main() {}
